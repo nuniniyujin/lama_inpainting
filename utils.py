099e56981156b908d4e54ec0591389555426b3d4
Gabriel Barbutti <gabrielbarbutti@gmail.com>
2022-02-05 22:51:16 +0100

    Create directories if they don't exist

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import datetime as dt
 import matplotlib.pyplot as plt
 
@@ -6,12 +7,20 @@ from torchvision.utils import save_image
 
 
 def saving_model(model,epoch,batch,base_dir):
+    if not os.path.exists(base_dir):
+        # Create a new directory because it does not exist 
+        os.makedirs(base_dir)
+
     now = dt.datetime.now()
     dt_string = now.strftime("%d-%m-%Y %Hh%M")
     torch.save(model.state_dict(), f"{base_dir}/model{epoch}epochs{batch}batch{dt_string}.pth")
     print(f"model {epoch} epochs {batch} batch {dt_string}.pth saved")
 
 def saving_image(image_reconstructed,epoch,base_dir):
+    if not os.path.exists(base_dir):
+        # Create a new directory because it does not exist 
+        os.makedirs(base_dir)
+
     now = dt.datetime.now()
     dt_string = now.strftime("%d-%m-%Y %Hh%M")
     save_image(image_reconstructed,f"{base_dir}/image{epoch}epochs{dt_string}.png")
